3925e0f600a972d24f6ddf01a267e592a30c5640
LDWM <97044110+LDWM@users.noreply.github.com>
2023-04-20 20:56:16 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting ='Good evening';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon' 
+} else if (hourNow > 0) {
+    greeting = 'Good morning'
+} else {
+    greeting = 'Welcome';
+};
+
+var el = document.getElementById('new');
+el.innerHTML = '<h2>' + greeting + '</h2>';
+document.getElementById('new').getAttribute('class');
