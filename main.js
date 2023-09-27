11471e0354accfbd16107ce5731238a1c0ba0dba
LDWM <97044110+LDWM@users.noreply.github.com>
2023-09-27 16:53:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,20 +3,20 @@ $(document).ready(function(){
 });
 
 
-//var today = new Date();
-//var hourNow = today.getHours();
-//var greeting;
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
 
-/if (hourNow > 18) {
-//    greeting ='Good evening';
-//} else if (hourNow > 12) {
-//    greeting = 'Good afternoon' 
-//} else if (hourNow > 0) {
-//    greeting = 'Good morning'
-//} else {
- //   greeting = 'Welcome';
-//};
+if (hourNow > 18) {
+    greeting ='Good evening';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon' 
+} else if (hourNow > 0) {
+    greeting = 'Good morning'
+} else {
+    greeting = 'Welcome';
+};
 
-//var el = document.getElementById('new');
-//el.innerHTML = '<h2>' + greeting + '</h2>';
-//document.getElementById('new').getAttribute('class');
+var el = document.getElementById('new');
+el.innerHTML = ('<h2>' + greeting + '</h2>');
+document.getElementById('new').getAttribute('class');
