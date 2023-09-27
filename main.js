9854315d256764f4911322a50b1e971af3811654
LDWM <97044110+LDWM@users.noreply.github.com>
2023-09-27 16:50:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,20 +2,21 @@ $(document).ready(function(){
     $('#emblem').fadeIn('slow');
 });
 
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
 
-if (hourNow > 18) {
-    greeting ='Good evening';
-} else if (hourNow > 12) {
-    greeting = 'Good afternoon' 
-} else if (hourNow > 0) {
-    greeting = 'Good morning'
-} else {
-    greeting = 'Welcome';
-};
+//var today = new Date();
+//var hourNow = today.getHours();
+//var greeting;
 
-var el = document.getElementById('new');
-el.innerHTML = '<h2>' + greeting + '</h2>';
-document.getElementById('new').getAttribute('class');
+/if (hourNow > 18) {
+//    greeting ='Good evening';
+//} else if (hourNow > 12) {
+//    greeting = 'Good afternoon' 
+//} else if (hourNow > 0) {
+//    greeting = 'Good morning'
+//} else {
+ //   greeting = 'Welcome';
+//};
+
+//var el = document.getElementById('new');
+//el.innerHTML = '<h2>' + greeting + '</h2>';
+//document.getElementById('new').getAttribute('class');
