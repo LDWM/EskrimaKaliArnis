dfcdfd63347bc43c3bb07d963fa6ef9f015507d3
LDWM <97044110+LDWM@users.noreply.github.com>
2023-12-22 14:33:48 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 $(document).ready(() => {
     $('#emblem').fadeIn({easing: "linear"}, 2000);
     $('#warriorslogo').fadeIn(2000);
+    $('#FMAHeading').show("slide", {distance: 500}, 1750);
+    $('#EKAHeading').show("slide", {direction: "right", distance: 500}, 2000);
 });
 
+$('#warriorslogo').click(function() {
+    $('#warriorslogo').fadeOut(1000);
+    $('#warriorslogo').fadeIn(1000);
+})
 
 var today = new Date();
 var hourNow = today.getHours();
