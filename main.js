43e4f941fd09b17227cbd99405cb7585aa7c98af
LDWM <97044110+LDWM@users.noreply.github.com>
2023-12-23 17:46:39 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 $(document).ready(() => {
     $('#emblem').fadeIn({easing: "linear"}, 2000);
     $('#warriorslogo').fadeIn(2000);
-    $('#FMAHeading').show("slide", {distance: 500}, 1750);
-    $('#EKAHeading').show("slide", {direction: "right", distance: 500}, 2000);
+    //$('#FMAHeading').show("slide", {distance: 500}, 1750);
+    //$('#EKAHeading').show("slide", {direction: "right", distance: 500}, 2000);
+    $('#FMAHeading').show("drop", {direction: "up", distance: 500}, 1750);
+    $('#EKAHeading').show("drop", {direction: "up", distance: 500}, 2000);
 });
 
 $('#warriorslogo').click(function() {
